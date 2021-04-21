b4772fcf9dd5c5ebdb9ab0f45b5b849bb0a49eb8
Ivo Ferreira Villa <ivofvilla@hotmail.com>
2021-04-21 13:04:17 -0300

    teste de contrutor

---
+++ Alura.LeilaoOnline.Testes/LanceCtor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.Testes
+{
+    public class LanceCtor
+    {
+        [Fact]
+        public void LancaArgumentExceptionDadoValorNegativo()
+        {
+            //arranje
+            var valorNegativo = -100;
+
+            //assert
+            Assert.Throws<ArgumentException>(
+                //Act
+                () => new Lance(null, valorNegativo)
+            );
+        }
+    }
+}
